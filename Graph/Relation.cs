d69496e3fa43e744aa7c3097f6dbd3ea1d39c69d
Fluctuationqt <Outrageousxqt@gmail.com>
2018-01-20 16:24:04 +0200

    Add files via upload

---
+++ Graph/Relation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphCSharp
+{
+    class Relation
+    {
+        public Node From { get; }
+        public Node To { get; }
+        public float Weight { get; }
+
+        public Relation(Node from, Node to, float weight)
+        {
+            From = from;
+            To = to;
+            Weight = weight;
+        }
+    }
+}
